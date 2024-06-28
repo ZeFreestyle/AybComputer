ba1c5f8b616230d07e47634c2f7c02d9a67aa6bf
ㅤ <44614193+ZeFreestyle@users.noreply.github.com>
2024-06-28 07:02:19 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,1 +1,11 @@
+RegisterCommand('openbrowser', function()
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'open'
+    })
+end)
+
+RegisterNUICallback('close', function()
+    SetNuiFocus(false, false)
+end)
 
