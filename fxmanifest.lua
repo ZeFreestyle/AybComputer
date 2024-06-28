b9200d3f559cbfb8370de25e3c319af56c2368af
ㅤ <44614193+ZeFreestyle@users.noreply.github.com>
2024-06-28 07:03:24 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Ayoub31tls'
+description 'Ordinateur avec navigateur'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
